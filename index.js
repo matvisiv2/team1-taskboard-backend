ccbe01e96cd4fb2de7bcdd10d7799a37b795f174
matvisiv2 <matvisiv2@gmail.com>
2025-10-10 22:58:27 +0300

    server/db startup fix

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const db = require('./db');
+
 const userRouter = require('./routes/user.routes');
 const boardRouter = require('./routes/board.routes');
 
-const PORT = process.env.PORT || 8080;
+const SERVER_PORT = process.env.SERVER_PORT || 8080;
+const DB_PORT = process.env.DB_PORT || 5432;
 
 const app = express();
 
@@ -10,4 +13,20 @@ app.use(express.json());
 app.use('/api', userRouter);
 app.use('/api', boardRouter);
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+async function startServer () {
+	try {
+		await db.query('SELECT NOW()');
+		console.log(
+			`DB running on port ${DB_PORT}. Connected to PostgreSQL successfully`,
+		);
+
+		app.listen(SERVER_PORT, () =>
+			console.log(`server started on port ${SERVER_PORT}`),
+		);
+	} catch (err) {
+		console.error(`DB connection failed: ${err.message}`);
+		process.exit(1);
+	}
+}
+
+startServer();
