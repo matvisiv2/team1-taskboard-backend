1eed159dea58adf99ec30fdae919d509aee70e55
matvisiv2 <matvisiv2@gmail.com>
2025-10-19 01:40:31 +0300

    auth/signup, auth/signin

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,71 +1,76 @@
-const db = require('../db');
 const jwt = require('jsonwebtoken');
-const t_user = require('../db/models/tuser');
+const bcrypt = require('bcrypt');
+
+const db = require('../db');
+const tuser = require('../db/models/tuser');
+
+const generateToken = payload => {
+	return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+};
 
 class AuthController {
 	async signUp (req, res) {
 		try {
 			const body = req.body;
-			// TODO: hash the password
-			// const token = jwt.sign(
-			// 	{
-			// 		email: body.email,
-			// 		password: body.password,
-			// 	},
-			// 	'secret1',
-			// );
-
-			// TODO: check if user alreaty exist
-			// const auth = await db.query('SELECT COUNT(*) FROM user WHERE email=$1', [
-			// 	body.email,
-			// ]);
-
-			const newUser = await t_user.create({
+			const newUser = await tuser.create({
 				userType: '1',
 				firstName: body.firstName,
 				lastName: body.lastName,
 				email: body.email,
-				// TODO: password hash instead of clear password
-				passwordHash: body.password,
+				password: body.password,
+				confirmPassword: body.confirmPassword,
 			});
 
-			if (!newUser) {
-				return res.status(500).json({
-					status: 'fail',
-					message: 'Fail to create user.',
-				});
-			}
+			const result = newUser.toJSON();
 
-			return res.json({ stauts: 'success', data: newUser });
+			delete result.password;
+			delete result.deletedAt;
+
+			result.token = generateToken({
+				id: result.id,
+			});
+
+			return res.json({ status: 'success', result });
 		} catch (err) {
 			console.log(err);
-			return res.status(500).json({ stauts: 'fail', error: 'Database error' });
+			return res
+				.status(500)
+				.json({ status: 'fail', message: 'Database error' });
 		}
 	}
 	async signIn (req, res) {
 		try {
 			const { email, password } = req.body;
 
-			const token = jwt.sign(
-				{
-					email,
-					password,
-				},
-				'secret1',
-			);
-
-			const auth = await db.query(
-				'SELECT COUNT(*) FROM users WHERE email=$1 AND passwordHash=$2',
-				[email, passwordHash],
-			);
-			if (auth.rowCount > 0) {
-				res.json({ success: true, token: token });
-			} else {
-				res.status(401).json({ error: 'email and password do not match.' });
+			if (!email || !password) {
+				return res.status(400).json({
+					status: 'fail',
+					message: 'Please provide email and password',
+				});
 			}
+
+			const result = await tuser.findOne({ where: { email } });
+
+			if (!result || !(await bcrypt.compare(password, result.password))) {
+				return res.status(401).json({
+					status: 'fail',
+					message: 'Incorrect mail or password',
+				});
+			}
+
+			const token = generateToken({
+				id: result.id,
+			});
+
+			return res.json({
+				status: 'success',
+				token,
+			});
 		} catch (err) {
 			console.log(err);
-			res.status(500).json({ error: 'Database error' });
+			res.status(500).json({ message: 'Database error' });
 		}
 	}
 }
